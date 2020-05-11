43f7a37251302a890e141dc2e8db822c1e3e5737
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 17:04:28 -0500

    first commit

---
+++ displayLocalCertificates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace displayLocalCertificates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+
+            store.Open(OpenFlags.ReadOnly);
+
+            Console.WriteLine("Friendly Name \t\t\t\t\t Expiration Date");
+            foreach (X509Certificate2 certificate in store.Certificates)
+                Console.WriteLine("{0}\t{1}", certificate.FriendlyName, certificate.NotAfter);
+            Console.ReadKey();
+
+        }
+    }
+}
